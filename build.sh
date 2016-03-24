c91ff785ba433da9084ce86bcf7f9a3432e9859e
djtm <djtm@users.noreply.github.com>
2016-03-24 15:52:20 +0000

    build.sh: typo fix, version as parameter

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -x
 
-version=0.12.20
+version=$1
 arch="386 amd64 arm arm64 ppc64 ppc64le"
 repo="djtm/syncthing-scratch"
 
@@ -11,7 +12,7 @@ gpg --keyserver pool.sks-keyservers.net --recv-keys "${SYNCTHING_GPG_KEY}" \
 # prepare arch version
 for i in $arch; do
 sh prepare.sh $i $version
-docker build -t ${repo}-${i}:${vervsion} .
+docker build -t ${repo}-${i}:${version} .
 docker build -t ${repo}-${i} .
 docker push ${repo}-${i}:${version}
 docker push ${repo}-${i}
