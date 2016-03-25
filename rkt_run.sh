dd9954d550c8418e6cccdf848e43933f5d718b91
djtm <djtm@users.noreply.github.com>
2016-03-25 12:55:50 +0100

    rkt_run: use private network, forward ports

--- rkt_run.sh
+++ rkt_run.sh
@@ -2,21 +2,22 @@
 
 [ -x ./rkt ] || echo Get rkt from https://github.com/coreos/rkt/releases and run this script inside the directory with the rkt binary.
 
-CONFIG="$HOME/.config/syncthing"
+CONFIG="$HOME/.config/syncthing-rkt"
 mkdir -p "$CONFIG" $HOME/Sync
 
-
-sudo ./rkt run --interactive  --insecure-options=image --net=host --dns 8.8.8.8 --hostname $(hostname)-rkt-syncthing \
+sudo ./rkt run --interactive  --insecure-options=image --dns 8.8.8.8 --hostname $(hostname)-rkt-syncthing \
+--port 8384-tcp:8384 --port 22000-tcp:22000 \
 --volume=ssl,kind=host,source=/etc/ssl/certs,readOnly=true --mount volume=ssl,target=/etc/ssl/certs \
 --volume=sync,kind=host,source=$HOME/Sync --mount volume=sync,target=/root/Sync \
 --volume config,kind=host,source="$CONFIG" --mount volume=config,target=/root/.config/syncthing \
 docker://djtm/syncthing-scratch-amd64 \
 --cpu 200m --memory 500M \
--- $*
+-- -gui-address="http://0.0.0.0:8384" $*
 
 # currently only amd64 supported.
 # cpu and memory must stand after the container name
 # https://coreos.com/rkt/docs/latest/subcommands/run.html
+# namespacing does not yet seem to work --private-users --no-overlay \
 
 pidof syncthing 2>/dev/null && \
 ionice -c 3 -p $(pidof syncthing) && \
